7bdf53b8168eaf2ef14de2384af2f73755d672ae
rafael1249 <89105388+rafael1249@users.noreply.github.com>
2021-10-14 16:56:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+
+function setup(){
+  createCanvas(1500, 800);
+}
+
+
+function draw(){
+  background(220);
+
+  textSize(30);
+  text("ingredientes:",500,200);
+  textSize(20);
+  text(" Um pacote de marshmallow branco, 2 barras de chocolate preto",500,250)
+  textSize(30);
+  text("Modo de preparo: ",500,350)
+  textSize(20);
+  text("Derreta o chocolate, coloque o chocolate derretido em uma forma circular, acrecente",500,400);
+  text("os marshmallows, coloque no forno em 200° por 10 minutos, e pronto seus marshmallows estaram",500,430);
+  text("prontos para ser comidos. ",500,460)
+  
+}
+
